2c2b72728cfa59fc34716d83d94dd61cea78fac4
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-25 14:59:12 +0200

    first commit

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.admin')
+
+@section('content')
+    <form method="POST" action="{{ route('admin.projects.store') }}">
+
+        @csrf
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo:</label>
+            <input type="text" class="form-control @error('title') is-invalid @enderror " id="title" name="title" value="{{old('title')}}">
+            @error('title')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+
+
+        <div class="mb-3">
+            <label for="description" class="form-label">Descrizione del progetto:</label>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{old('description')}}</textarea>
+            @error('description')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="type_id" class="form-label">Seleziona tipologia</label>
+            <select class="form-select @error('type_id') is-invalid @enderror" name="type_id" id="type_id">
+                <option @selected(old('type_id')=='') value="">Nessuna tipologia</option>
+                @foreach ($types as $type)
+                    <option @selected(old('type_id')==$type->id) value="{{$type->id}}">{{$type->name}}</option>
+                @endforeach
+            </select>
+            @error('type_id')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Salva</button>
+
+    </form>
+@endsection
